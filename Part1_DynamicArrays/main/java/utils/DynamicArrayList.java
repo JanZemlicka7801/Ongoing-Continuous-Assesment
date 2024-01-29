9a81301dbf389872dbfba0384660fa1c87c51790
JanZemlicka7801 <jan.zemlicka1@gmail.com>
2024-01-29 17:27:11 +0000

    Finished get() method which can provide a data from specific position in array.

--- Part1_DynamicArrays/main/java/utils/DynamicArrayList.java
+++ Part1_DynamicArrays/main/java/utils/DynamicArrayList.java
@@ -14,4 +14,16 @@ public class DynamicArrayList {
     public int size(){
         return size;
     }
+
+    /*
+    A method called get() that takes a single parameter, an int representing the position from which data should be
+    retrieved. The position should be validated and invalid positions handled accordingly.
+    If the position is valid, the method should return the data (a String) in the position specified.
+     */
+
+    public String get(int position){
+        if (position < 0 || position >= size)
+            throw new ArrayIndexOutOfBoundsException("Array Index is out of bounds.");
+        return this.array[position];
+    }
 }
